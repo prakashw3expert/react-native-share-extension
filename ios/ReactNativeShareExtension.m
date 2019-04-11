c0ab80a7e51e531a6f23b46c174a8d5924c023e0
Prakash Saini <prakashw3expert@gmail.com>
2019-04-11 13:26:47 +0530

    Update ReactNativeShareExtension.m

--- ios/ReactNativeShareExtension.m
+++ ios/ReactNativeShareExtension.m
@@ -1,8 +1,10 @@
 #import "ReactNativeShareExtension.h"
 #import "React/RCTRootView.h"
 #import <MobileCoreServices/MobileCoreServices.h>
+#import <CoreLocation/CoreLocation.h>
 
 #define URL_IDENTIFIER @"public.url"
+#define CARD @"public.content"
 #define IMAGE_IDENTIFIER @"public.image"
 #define TEXT_IDENTIFIER (NSString *)kUTTypePlainText
 
@@ -43,14 +45,6 @@ RCT_EXPORT_METHOD(close) {
 
 
 
-RCT_EXPORT_METHOD(openURL:(NSString *)url) {
-  UIApplication *application = [UIApplication sharedApplication];
-  NSURL *urlToOpen = [NSURL URLWithString:[url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
-  [application openURL:urlToOpen options:@{} completionHandler: nil];
-}
-
-
-
 RCT_REMAP_METHOD(data,
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
@@ -70,60 +64,117 @@ RCT_REMAP_METHOD(data,
 - (void)extractDataFromContext:(NSExtensionContext *)context withCallback:(void(^)(NSString *value, NSString* contentType, NSException *exception))callback {
     @try {
         NSExtensionItem *item = [context.inputItems firstObject];
+        
         NSArray *attachments = item.attachments;
 
         __block NSItemProvider *urlProvider = nil;
         __block NSItemProvider *imageProvider = nil;
         __block NSItemProvider *textProvider = nil;
-
-        [attachments enumerateObjectsUsingBlock:^(NSItemProvider *provider, NSUInteger idx, BOOL *stop) {
+        for (NSItemProvider *provider in attachments){
+            NSLog(@"%@", provider);
             if([provider hasItemConformingToTypeIdentifier:URL_IDENTIFIER]) {
                 urlProvider = provider;
-                *stop = YES;
-            } else if ([provider hasItemConformingToTypeIdentifier:TEXT_IDENTIFIER]){
-                textProvider = provider;
-                *stop = YES;
-            } else if ([provider hasItemConformingToTypeIdentifier:IMAGE_IDENTIFIER]){
-                imageProvider = provider;
-                *stop = YES;
             }
-        }];
 
-        if(urlProvider) {
+            if([provider hasItemConformingToTypeIdentifier:TEXT_IDENTIFIER]) {
+                NSLog(@" card %@", provider);
+                textProvider = provider;
+            }
+        }
+
+        if(urlProvider && textProvider) {
+            __block  NSString *urlString = @"";
             [urlProvider loadItemForTypeIdentifier:URL_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
                 NSURL *url = (NSURL *)item;
-
+                urlString = [url absoluteString];
+            }];
+            [textProvider loadItemForTypeIdentifier:TEXT_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    NSString *text = (NSString *)item;
+                    CLLocationCoordinate2D location = [self getLocationFromAddressString:text];
+                NSString *value = [NSString stringWithFormat:@"%@,%f,%f", urlString, location.latitude, location.longitude];
+                    if(callback) {
+                        callback(value, @"text/plain", nil);
+                    }
+                }];
+        }
+        else if(urlProvider) {
+            [urlProvider loadItemForTypeIdentifier:URL_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                NSURL *url = (NSURL *)item;
                 if(callback) {
                     callback([url absoluteString], @"text/plain", nil);
                 }
             }];
-        } else if (imageProvider) {
-            [imageProvider loadItemForTypeIdentifier:IMAGE_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
-                NSURL *url = (NSURL *)item;
-
-                if(callback) {
-                    callback([url absoluteString], [[[url absoluteString] pathExtension] lowercaseString], nil);
-                }
-            }];
-        } else if (textProvider) {
-            [textProvider loadItemForTypeIdentifier:TEXT_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
-                NSString *text = (NSString *)item;
-
-                if(callback) {
-                    callback(text, @"text/plain", nil);
-                }
-            }];
         } else {
-            if(callback) {
-                callback(nil, nil, [NSException exceptionWithName:@"Error" reason:@"couldn't find provider" userInfo:nil]);
+            [attachments enumerateObjectsUsingBlock:^(NSItemProvider *provider, NSUInteger idx, BOOL *stop) {
+                if([provider hasItemConformingToTypeIdentifier:URL_IDENTIFIER]) {
+                    urlProvider = provider;
+                    *stop = YES;
+                } else if ([provider hasItemConformingToTypeIdentifier:TEXT_IDENTIFIER]){
+                    textProvider = provider;
+                    *stop = YES;
+                } else if ([provider hasItemConformingToTypeIdentifier:IMAGE_IDENTIFIER]){
+                    imageProvider = provider;
+                    *stop = YES;
+                }
+            }];
+
+            if(urlProvider) {
+                [urlProvider loadItemForTypeIdentifier:URL_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    NSURL *url = (NSURL *)item;
+                    if(callback) {
+                        callback([url absoluteString], @"text/plain", nil);
+                    }
+                }];
+            } else if (imageProvider) {
+                [imageProvider loadItemForTypeIdentifier:IMAGE_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    NSURL *url = (NSURL *)item;
+                    if(callback) {
+                        callback([url absoluteString], [[[url absoluteString] pathExtension] lowercaseString], nil);
+                    }
+                }];
+            } else if (textProvider) {
+                [textProvider loadItemForTypeIdentifier:TEXT_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    NSString *text = (NSString *)item;
+                    if(callback) {
+                        callback(text, @"text/plain", nil);
+                    }
+                }];
+            } else {
+                if(callback) {
+                    callback(nil, nil, [NSException exceptionWithName:@"Error" reason:@"couldn't find provider" userInfo:nil]);
+                }
             }
         }
+        
+
+        
     }
     @catch (NSException *exception) {
         if(callback) {
             callback(nil, nil, exception);
         }
     }
+}
+
+-(CLLocationCoordinate2D) getLocationFromAddressString: (NSString*) addressStr {
+    double latitude = 0, longitude = 0;
+    NSString *esc_addr =  [addressStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString *req = [NSString stringWithFormat:@"https://maps.google.com/maps/api/geocode/json?sensor=false&address=%@&key=GOOGLE_API_KEY", esc_addr];
+    NSString *result = [NSString stringWithContentsOfURL:[NSURL URLWithString:req] encoding:NSUTF8StringEncoding error:NULL];
+    if (result) {
+        NSScanner *scanner = [NSScanner scannerWithString:result];
+        if ([scanner scanUpToString:@"\"lat\" :" intoString:nil] && [scanner scanString:@"\"lat\" :" intoString:nil]) {
+            [scanner scanDouble:&latitude];
+            if ([scanner scanUpToString:@"\"lng\" :" intoString:nil] && [scanner scanString:@"\"lng\" :" intoString:nil]) {
+                [scanner scanDouble:&longitude];
+            }
+        }
+    }
+    CLLocationCoordinate2D center;
+    center.latitude=latitude;
+    center.longitude = longitude;
+    return center;
+
 }
 
 @end
